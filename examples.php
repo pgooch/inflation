a6e70a756b6ccca1d50c9cc276cae384b7e8616c
Phillip Gooch <phillip.gooch@gmail.com>
2014-08-18 16:58:14 -0700

    Added local calculations.
    No longer do you have to wait for the bls.gov site to give you your
    answer. Also can do to-the-month calculations.

---
+++ examples.php
@@ -0,0 +1,120 @@
+<html>
+<head>
+	<title>Inflation Class Examples</title>
+	<style>
+		p{
+			max-width:589px;
+			margin:1.5em auto;
+		}
+		td, th {
+		    padding: .5em;
+		}
+		tr:nth-child(even) {
+		    background: #eee;
+		}
+		table {
+		    border-collapse: collapse;
+		    margin: 1em auto;
+		}
+		body {}
+		caption {
+		    font-size: 1.5em;
+		}
+	</style>
+</head>
+<body>
+<?php
+require_once('./inflation.php');
+$inflation = new inflation();
+?>
+
+<p>
+	The cost of a 1962 VW Beetle De Luxe (With White Wall Tires and Leatherette Interior) was $1,755.61, that would be $<?= number_format($inflation->adjust(1755.61,1962),2) ?> today.<br/>
+</p>
+
+<p>
+	Despite the intended price point of $12,000 the DeLorean DMC-12 had an initial sticker price of $25,000, that would be like $<?= number_format($inflation->adjust(25000,1981,1985),2) ?> when Doc Brown took his to 1955 and would have been like $<?= number_format($inflation->adjust(25000,1981,1955),2) ?> back then.
+</p>
+
+<p>
+	The cost of a Ferrari Testarossa in 1989 was $181,000, thats like $<?= number_format($inflation->adjust(181000,1989),2) ?> today.
+</p>
+
+<p>
+	The cost of an original Apple 1 computer was $666.66 when it was released in 1976 that would be like $<?= number_format($inflation->adjust(666.66,1976,1998),2) ?> twenty-two years later when the first iMac was released.<br/>
+</p>
+
+<table>
+	<caption>Nintendo Gaming Systems</caption>
+	<tr>
+		<th>System</th>
+		<th>Release Date</th>
+		<th>Original Price</th>
+		<th>Adjusted Price</th>
+	</tr>
+	<?php
+	foreach(array(
+		'Nintendo Entertainment System' => array(
+			'released' => '10/1985',
+			'original_price' => 299.00,
+		),
+		'Game Boy' => array(
+			'released' => '7/1989',
+			'original_price' => 89.99,
+		),
+		'Super Nintendo Entertainment System' => array(
+			'released' => '8/1991',
+			'original_price' => 199.00,
+		),
+		'Virtual Boy' => array(
+			'released' => '8/1995',
+			'original_price' => 180.00,
+		),
+		'Nintendo 64' => array(
+			'released' => '9/1996',
+			'original_price' => 199.99,
+		),
+		'Game Boy Color' => array(
+			'released' => '11/1998',
+			'original_price' => 79.95,
+		),
+		'Game Boy Advance' => array(
+			'released' => '06/2001',
+			'original_price' => 149.99,
+		),
+		'Gamecube' => array(
+			'released' => '11/2001',
+			'original_price' => 199.00,
+		),
+		'Nintendo DS' => array(
+			'released' => '11/2004',
+			'original_price' => 149.99,
+		),
+		'Wii' => array(
+			'released' => '12/2006',
+			'original_price' => 249.99,
+		),
+		'Nintendo 3DS' => array(
+			'released' => '2/2011',
+			'original_price' => 249.99,
+		),
+		'WiiU (Basic Set)' => array(
+			'released' => '11/2012',
+			'original_price' => 299.99,
+		),
+		'WiiU (Delux Set)' => array(
+			'released' => '11/2012',
+			'original_price' => 349.99,
+		),
+	) as $system => $data){
+		echo '<tr>
+			<td>'.$system.'</td>
+			<td>'.$data['released'].'</td>
+			<td>'.$data['original_price'].'</td>
+			<td>'.$inflation->adjust($data['original_price'],$data['released']).'</td>
+		</tr>';
+	}
+	?>
+</table>
+</body>
+</html>
--- readme.md
+++ readme.md
@@ -1,19 +1,18 @@
 # Inflation Class
 
-A class that will get an amount, adjusted for inflation _or_ deflation, by using the US Bureau of Labor Statistics calculator.
+A simple PHP function to either calculate inflation locally or to scrape the http://data.bls.gov/ inflation calculator for the adjustment.
 
 ### Requirements
-This function requires cURL access.
+This function requires cURL access if you wish to scrape the bls.gov calculator for your answer.
 
 ### Usage
-After including the class file you can all the static function with `inflation::adjust($amount,$in_year,$like_year)` where; `$amount` is the value you want to adjust, `$in_year` is the year that amount was in, and `$like_year` is the year you to get the adjustment for. You can leave `like_year` off to always pull the adjustment for the current year. For example:
+After includeing the class you either main adjustment function:
 
-	// The cost of the Apple 1, 10 years later.
-	$wozers = inflation::adjust(666.66,1976,1986);
+- `adjust($amount,$in_year,[$like_year])` will adjust the `$ammount` as if was in `$in_year` like it was `$like_year`. With this function you do not need to call the `$like_year`, if you lave it absent it will automatically fill it for the latest possible date (the last month of the last year of data). This function uses the internal CPI data table and therefore can do inflation down to the month. Various month formats should be usable, but it only supports month and year combinations (like 10/1999 or 1987-4). This will return the adjusted value of 0 if there was an error. Errors throw exceptions that can be caught.
 
-In this case th variable `$wozers` would contain `1,284.11`.
+- `grabAdjustment($ammount,$in_year,[$like_year])`  will adjust the `$ammount` as if was in `$in_year` like it was `$like_year`. Like with the above if you omit the `$like_year` it will pull for the current year. Since this one puuls from the CPI calculator is cannot do to-the-month adjustments. This will return the adjusted amount or an error message. This is a static function so it can be called directly, but it is also substanially slower beacuse it has to cURL the bls.gov website for the infomration.
 
-	// The cost of a 1962 VW Beetle De Luxe (With White Wall Tires and Leatherette Interior) today
-	$putput = inflation::adjust(1755.61,1962);
+Example useage can be found in examples.php. 
 
-And in this one the varible `$putput` would contain `13,829.79`.
+### About the data
+The data used in internal calculations is gathered from http://www.bls.gov/cpi/cpid1406.pdf Table 24. It is stored in a multi-dimensional array in a `array($year => array($avg, $jan, $feb...),...` format. This class will most likely not be updated on a monthly basis as new information is added, but adding new months of data is simple. If you do go ahead and add new data submit a pull request and I'll incorperate it into the main repository.
