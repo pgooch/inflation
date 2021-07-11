9fcf0b60af79d7befab96e2bf5e9e45c149dac5b
Phillip Gooch <phillip.gooch@gmail.com>
2021-07-10 21:57:35 -0700

    Added message to cli when no params passed, updated data.

--- inflation-cli.php
+++ inflation-cli.php
@@ -2,11 +2,13 @@
 require_once(realpath(__DIR__.'/inflation.php'));
 $inflation = new inflation();
 
-if( count($argv) < 3 ){
-	echo 'Expected at least arguments; price and then date but recieved '.(count($argv)-1).'.';
+if( count($argv) === 1){
+	echo 'Inflation data last updated '.$inflation->data_updated().', pass price, original date, and an option target date to get adjustment.';
 	exit;
-}
-if( count($argv) > 4 ){
+}else if( count($argv) < 3 ){
+	echo 'Expected at least 3 arguments; price and then date but recieved '.(count($argv)-1).'.';
+	exit;
+}else if( count($argv) > 4 ){
 	echo 'Expected no more than 4 arguments; price, original date, and now date but recieved '.(count($argv)-1).'.';
 	exit;
 }
